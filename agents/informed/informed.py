96dfdf01cfa3e352582eafae138cbd4683469148
LiorMoshe <liormoshe0909@gmail.com>
2019-03-29 15:04:30 +0300

    Docs

--- agents/informed/informed.py
+++ agents/informed/informed.py
@@ -2,4 +2,8 @@ from ..player import Player
 
 
 class Informed(Player):
+    """
+    This is where we will implement all the functionality that is relevant to the informed players
+    of the werewolf group.
+    """
     pass
