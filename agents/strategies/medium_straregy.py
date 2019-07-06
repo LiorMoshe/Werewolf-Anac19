cece96c3d6de6f4394646b88ab5b0327db1808bc
Roxnya <chernroni@gmail.com>
2019-07-06 11:58:56 +0300

    bug fixes

--- agents/strategies/medium_straregy.py
+++ agents/strategies/medium_straregy.py
@@ -18,7 +18,7 @@ class MediumStrategy(TownsFolkStrategy):
     PROB_OF_COMINGOUT = 0.6
 
     def __init__(self, agent_indices, my_index, role_map, statusMap, player_perspective):
-        super().__init__(agent_indices, my_index, role_map)
+        super().__init__(agent_indices, my_index, role_map, player_perspective)
 
         self.my_index = my_index
         self._divined_agents = {}
@@ -101,6 +101,7 @@ class MediumStrategy(TownsFolkStrategy):
                     coop_with_heat = max(cooperators, key=lambda c: 
                                                           self._player_perspective.under_heat_value[c])
                     return str(coop_with_heat)
+            return super().vote()
         else:
             v = super().vote()
             print("v is {}".format(v))
--- agents/uninformed/medium.py
+++ agents/uninformed/medium.py
@@ -1,5 +1,5 @@
 from agents.uninformed.villager import Villager
-from agents.strategies.agent_strategy import TownsFolkStrategy
+from agents.strategies.medium_straregy import MediumStrategy
 
 # gets the identity of the voted player each day
 
